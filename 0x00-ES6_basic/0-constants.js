2bc462dfa58fefcd142f9a8106eb8cfd90cca45b
Wizsociety <iankimathi14@gmail.com>
2024-10-11 22:00:18 +0300

    js basics

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,9 +1,11 @@
 function taskFirst() {
 	const task = "I prefer constant when i can";
+	alert(task);
 	return task;
 }
 
 function taskNext() {
 	let task = "but sometimes let is okay";
+	alert(task);
 	return task;
 }
