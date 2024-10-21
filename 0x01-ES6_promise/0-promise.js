342a50554229b99ba1442194c6998c3e24962d5d
Wizsociety <iankimathi14@gmail.com>
2024-10-21 12:00:54 +0300

    javascript ES6 Promise module

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // This Promise doesn't do anything specific, it just resolves immediately
+    resolve();
+  });
+}
